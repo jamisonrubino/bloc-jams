8a1fc4875da0e59f39dbd0ddc50bf02388c6536c
jkrubino <jamison.rubino@gmail.com>
2017-02-26 13:31:33 -0800

    Checkpoint 12 complete.

--- scripts/album.js
+++ scripts/album.js
@@ -50,8 +50,7 @@
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
-      + '  <td class="song-item-title">' + songName + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'      + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
       ;
@@ -82,13 +81,31 @@
      }
  };
 
- 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
 window.onload = function() {
    setCurrentAlbum(albumPicasso);
+	
+	songListContainer.addEventListener('mouseover', function(event) {		
+		// I don't fully understand where "event" is introduced, or why it acts like "this" -- what defines it?
+		
+      var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+      if (event.target.parentElement.className === 'album-view-song-item') {
+			event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+		}
+   });
+	
+   for (var i = 0; i < songRows.length; i++) {
+       songRows[i].addEventListener('mouseleave', function(event) {
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+       });
+   }
 	
 	var albums = [albumPicasso, albumMarconi, albumSpring];
 	var albumArt = document.getElementsByClassName('album-cover-art')[0];
 	var index = 1;
+	
 	albumArt.addEventListener("click", function(event) {
 		setCurrentAlbum(albums[index]);
 		index++;
