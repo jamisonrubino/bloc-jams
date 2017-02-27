9a358f2c6d86a891dd1a44a7947da083436c36a1
jkrubino <jamison.rubino@gmail.com>
2017-02-27 11:53:40 -0800

    Checkpoint 13 complete.

--- scripts/album.js
+++ scripts/album.js
@@ -58,6 +58,9 @@
      return template;
  };
 
+//
+//	============================= SET CURRENT ALBUM
+//
  var setCurrentAlbum = function(album) {
      // #1
      var albumTitle = document.getElementsByClassName('album-view-title')[0];
@@ -81,31 +84,119 @@
      }
  };
 
+//
+//	============================= FIND PARENT BY CLASS NAME
+//
+var findParentByClassName = function(element, targetClass) {
+    if (element) {
+		  if (element.parentElement) {
+			  var currentParent = element.parentElement;
+			  while (currentParent.className != targetClass && currentParent.className !== null) {
+					currentParent = currentParent.parentElement;
+			  }
+		  } else {
+			  console.log("No parent found.");
+		  }
+		  if (element.parentElement.className !== targetClass && currentParent.className !== targetClass) {
+			  console.log("No parent found with that class name.");
+		  }
+        return currentParent;
+    }
+};
+
+//
+// ============================= GET SONG ITEM
+//
+var getSongItem = function(element) {
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }  
+};
+
+//
+// ============================= CLICK HANDLER FOR PLAY/PAUSE BUTTON
+//
+var clickHandler = function(targetElement) {
+	var songItem = getSongItem(targetElement); 
+	// IF NO SONG PLAYING
+   if (currentlyPlayingSong === null) {
+       songItem.innerHTML = pauseButtonTemplate;
+       currentlyPlayingSong = songItem.getAttribute('data-song-number');
+	// IF CLICKED SONG CURRENTLY PLAYING
+   } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+       songItem.innerHTML = playButtonTemplate;
+       currentlyPlayingSong = null;
+	// IF PLAYING SONG NOT SONG CLICKED
+   } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+       var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+       currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+       songItem.innerHTML = pauseButtonTemplate;
+       currentlyPlayingSong = songItem.getAttribute('data-song-number');
+   }
+};
+
+//
+// ============================= WINDOW ONLOAD
+//
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
+var currentlyPlayingSong = null; // state of playing songs
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
 window.onload = function() {
    setCurrentAlbum(albumPicasso);
 	
-	songListContainer.addEventListener('mouseover', function(event) {		
-		// I don't fully understand where "event" is introduced, or why it acts like "this" -- what defines it?
-		
-      var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+	//
+	// ============================= ADD EVENT LISTENERS TO SONG ROWS
+	//
+				// MOUSEOVER EVENTS
+				// ADDED SONGITEM/SONGITEMNUMBER & IF STATEMENTS SO MOUSELEAVE WOULDN'T REVERT PAUSE BUTTON TO PLAY BUTTON
+	songListContainer.addEventListener('mouseover', function(event) { 		// what defines "event"?
+      var songItem = getSongItem(event.target);
+      var songItemNumber = songItem.getAttribute('data-song-number');
       if (event.target.parentElement.className === 'album-view-song-item') {
-			event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+      	if (songItemNumber !== currentlyPlayingSong) {
+				event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+      	} else if (songItemNumber === currentlyPlayingSong) {
+				event.target.parentElement.querySelector('.song-item-number').innerHTML = pauseButtonTemplate;
+			}
 		}
    });
-	
+				// MOUSELEAVE EVENTS
    for (var i = 0; i < songRows.length; i++) {
-       songRows[i].addEventListener('mouseleave', function(event) {
-             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
-       });
-   }
-	
+      songRows[i].addEventListener('mouseleave', function(event) {
+         var songItem = getSongItem(event.target);
+         var songItemNumber = songItem.getAttribute('data-song-number');
+         if (songItemNumber !== currentlyPlayingSong) {
+             songItem.innerHTML = songItemNumber;
+         }
+      });
+				// CLICK EVENTS
+   	songRows[i].addEventListener('click', function(event) {
+       	clickHandler(event.target);
+   	});
+	}
+		
 	var albums = [albumPicasso, albumMarconi, albumSpring];
 	var albumArt = document.getElementsByClassName('album-cover-art')[0];
 	var index = 1;
 	
+//
+// ============================= CYCLE ALBUMS ON ALBUM ART CLICK
+//
 	albumArt.addEventListener("click", function(event) {
 		setCurrentAlbum(albums[index]);
 		index++;
